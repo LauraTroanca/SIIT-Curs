a126f7374202992d2d3dc3e8a4c55fab48bad109
Troanca <Troanca@DESKTOP-3FDLD4Q>
2018-09-01 19:41:41 +0300

    My first program

---
+++ Java9CursLauraTroanca/src/java9curslauratroanca/Java9CursLauraTroanca.java
@@ -0,0 +1,22 @@
+/* comentariu pe mai multe linii
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java9curslauratroanca;
+
+/**
+ * aici e  comentariu javadoc
+ * @author Troanca
+ */
+public class Java9CursLauraTroanca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // comentariu pe o singura linie
+        System.out.println("Salut!");
+    }
+    
+}
