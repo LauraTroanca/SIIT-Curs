12cb14d6147127c88399bd22615e77d242b473fd
Troanca <Troanca@DESKTOP-3FDLD4Q>
2018-10-06 01:43:56 +0300

    Merge origin/master

---
+++ Java9CursLauraTroanca/src/polimorfism/Shape.java
@@ -0,0 +1,19 @@
+/*
+ * Shape class as a superclass
+ */
+package polimorfism;
+
+/**
+ *
+ * @author Troanca
+ */
+public class Shape {
+     // definition is keeping the text definition
+     private final String definition = "A shape";
+     
+     // method returns definition
+     public String getDefinition(){
+     return definition;
+ } 
+    
+}
