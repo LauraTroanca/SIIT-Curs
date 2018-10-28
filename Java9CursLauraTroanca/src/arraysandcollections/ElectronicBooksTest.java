a18ac85df4e46f56edb18a3dd69942fb0aa82e3f
Troanca <Troanca@DESKTOP-3FDLD4Q>
2018-10-28 16:50:46 +0200

    Merge origin/master

---
+++ Java9CursLauraTroanca/src/arraysandcollections/ElectronicBooksTest.java
@@ -0,0 +1,23 @@
+/*
+ * Program for Electronic Books
+ */
+package arraysandcollections;
+
+/**
+ *
+ * @author Troanca
+ */
+public class ElectronicBooksTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        ElectronicBooks book1 = ne
+        ElectronicBooks[] arrayForBooks = new ElectronicBooks[3];
+        
+    }
+    
+}
