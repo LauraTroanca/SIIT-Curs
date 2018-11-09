35d9a5696ec5920c5001d476db92dc194e158bd9
Troanca <Troanca@DESKTOP-3FDLD4Q>
2018-11-09 12:25:01 +0200

    EBooks

--- Java9CursLauraTroanca/src/arraysandcollections/ElectronicBooksTest.java
+++ Java9CursLauraTroanca/src/arraysandcollections/ElectronicBooksTest.java
@@ -3,6 +3,7 @@
  */
 package arraysandcollections;
 
+import static arraysandcollections.EBook.EBookReaders.KINDLE;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -23,6 +24,7 @@ public class ElectronicBooksTest {
         }
     }
    
+    /*
     // override equal method for hashSetForBooks to check by isbn
     @Override
     public boolean equals(Object o){
@@ -31,21 +33,30 @@ public class ElectronicBooksTest {
     //code
     return is;
     }
+    
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
             
-            
+    */        
             
     public static void main(String[] args) {
         
-        // create 3 objects
+        // create ElectronicBooks objects
         EBook book1 = new EBook("Amintiri din copilarie","Ion Creanga","1111111111111");
+        book1.setYearOfPublication(2015);
         EBook book2 = new EBook("Viata ca o prada","Marin Preda","1111111111112");
-        IBook book3 = new IBook("Panza de paianjen","Cella Serghi","1111111111113");        
+        IBook book3 = new IBook("Panza de paianjen","Cella Serghi","1111111111113"); 
+        EBook book4 = new EBook(KINDLE,"Amintiri din copilarie","Ion Creanga","1111111111111","mobi", 2010);
         
         // create and print an Array
-        ElectronicBooks[] arrayForBooks = new ElectronicBooks[3];
+        ElectronicBooks[] arrayForBooks = new ElectronicBooks[10];
         arrayForBooks[0]=book1;
         arrayForBooks[1]=book2;
         arrayForBooks[2]=book3;
+        arrayForBooks[3]=book4;
         System.out.println("----------Array------------");
         printArray(arrayForBooks);
         
@@ -54,6 +65,7 @@ public class ElectronicBooksTest {
         arrayListForBooks.add(book3);
         arrayListForBooks.add(book2);
         arrayListForBooks.add(2,book1);
+        arrayListForBooks.add(book4);
         System.out.println("----------ArrayList------------");
         for(ElectronicBooks str: arrayListForBooks){
 			System.out.println(str.getTitle()+", scris de "+ str.getAuthors());
@@ -64,6 +76,7 @@ public class ElectronicBooksTest {
         linkedListForBooks.add(book3);
         linkedListForBooks.add(book2);
         linkedListForBooks.addFirst(book1);
+        linkedListForBooks.add(book4);
         System.out.println("----------LinkedList------------");
         for(ElectronicBooks str: linkedListForBooks){
 			System.out.println(str.getTitle()+", scris de "+ str.getAuthors());
@@ -74,20 +87,18 @@ public class ElectronicBooksTest {
         hashSetForBooks.add(book1);
         hashSetForBooks.add(book2);
         hashSetForBooks.add(book3);
+        hashSetForBooks.add(book4);
         System.out.println("----------HashSet------------");
-        for(ElectronicBooks str: hashSetForBooks){
-			System.out.println(str.getTitle()+", scris de "+ str.getAuthors()+", isbn: "+ str.getIsbn());
+        System.out.println("There are "+ hashSetForBooks.size() + " books in this library:");
+        int x=1;
+        for(ElectronicBooks str: hashSetForBooks){       
+            System.out.println(x+ ". "+ str.getTitle()+", scris de "+ str.getAuthors()+", isbn: "+ str.getIsbn());
+            x++;
+            
         }
         
       
         
         
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        return hash;
-    }
-    
+    }   
 }
